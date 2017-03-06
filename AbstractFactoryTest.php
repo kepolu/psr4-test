b80eee164dcf700c51d08a35c4f7fe7733197a52
Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
2017-03-06 11:13:58 +0700

    fuk

--- AbstractFactoryTest.php
+++ AbstractFactoryTest.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use Foo\MahasiswaFactory;
--- autoload.php
+++ autoload.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Psr4ClassLoader.php';
 
 $autoload = Psr4ClassLoader::create();
